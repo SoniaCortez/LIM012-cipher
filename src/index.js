f085332a67fabb5245e4cf4c3eb195a779af689b
SoniaCortez <sonia.cortez@ditexgraf.com>
2020-02-14 12:03:12 -0500

    ejercicio boton

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+  console.log('Hola! Mucho gusto en conocerte`);
+});
