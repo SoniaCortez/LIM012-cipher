4300170f413c9490259dc1439b41842eeb50d9be
SoniaCortez <sonia.cortez@ditexgraf.com>
2020-02-17 10:34:08 -0500

    codigohtml

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import cipher from './cipher.js';
 
+const btn= document.getElementById('btn-desplazar')
+btn.addEventListener('click'()=>{(cuanto se va a desplazar);});
+
+
+
+
+
+
+
+
+
+
+
+
+
 console.log(cipher);
