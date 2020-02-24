dab266b7718f1347dfcf84ca1a02f08982098d16
SoniaCortez <sonia.cortez@ditexgraf.com>
2020-02-24 08:07:18 -0500

    funciones

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import cipher from './cipher.js';
-//botones de codificar y decodificar*/
+/*botones de codificar y decodificar*/
 const codiBtn = document.getElementById('boton1');
 const texto1 = document.getElementById('caja1');
 const texto2 = document.getElementById('caja2');
@@ -7,10 +7,10 @@ const desplazar = document.getElementById('offset');
 
 codiBtn.addEventListener('click',()=> {
   let box1 = texto1.value;
-  //capturando valor del boton de desplazmiento. Utilizar parseint para que sea numero*/
+  /*capturando valor del boton de desplazmiento. Utilizar parseint para que sea numero*/
   let box3 = parseInt(desplazar.value);
   let box2 = texto2;
-  //en la caja 2 va el resultado encriptado, por eso afrego aqui mi funcion*/
+  /*en la caja 2 va el resultado encriptado, por eso afrego aqui mi funcion*/
   box2.value = cipher.encode(box3, box1.toUpperCase());
 });
 
